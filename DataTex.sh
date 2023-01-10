63f9cb3dc8a7740c525779672d914764f4979da6
William Rezende <wrassis84@gmail.com>
2023-01-10 19:09:55 -0300

    feat: add list, add and remove options for DataTex.sh

--- DataTex.sh
+++ DataTex.sh
@@ -22,7 +22,7 @@
 ### VARIABLE DECLARATION :::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #
 DB_FILE="DataDB.txt" #| Database file
-LIB_FILE="LibTex.sh" #| Library file
+#LIB_FILE="LibTex.sh" #| Library file
 RED="\033[31;1m"     #|
 GREEN="\033[32;1m"   #|
 YELLOW="\033[33;1m"  #| Colors for output:
@@ -34,7 +34,7 @@ ESC="\033[m"         #| ESCAPE character
 ### MAIN CODE ::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 [ "$1" ] || {
   echo
-  echo "Usage: DataTex [ list | add | remove ]"
+  echo "Usage: ./DataTex [ list | add | remove ]"
   echo
   echo "   list   - List all system users"
   echo "   add    - Add a new user to the system"
@@ -44,41 +44,63 @@ ESC="\033[m"         #| ESCAPE character
 }
 source "$LIB_FILE" || {
   echo "${RED}WARN: Library functions not loaded!"
-  exit 1
+  return 1
 }
 
 case "$1" in
-  list ) Select_func   ;;
-   add ) echo -n "Type the new id: "
-         read id
-         [ "$id" ] || {
-           echo "${YELLOW}WARN: The id must be an integer. Try again!"
-           return
-         }
-         # search if id exists
-         Search_func && {
-           echo "${YELLOW}INFO: The id '$id' already exists on database!"
-           return
-         }
-         # if id not exists, go ahead
-         echo -n "Enter the new id: "
-         read id
-         echo -n "Enter complete name: "
-         read name
-         echo -n "Enter login [first name.last name]: "
-         read login
-         echo -n "Enter age [0-99]: "
-         read age
-         echo -n "Enter gender [male|female]: "
-         read gender
-         echo -n "Enter job title: "
-         read job
-         echo -n "Enter department: "
-         read department
-         echo
-         Insert_func "$id:$name:$login:$age:$gender:$job:$department"
-         echo
+  list )
+        Select_func
   ;;
-  pattern )    ;;
-  pattern )    ;;
+   add )
+        echo -n "Type the new id: "
+        read id
+        [ "$id" ] || {
+         echo "${YELLOW}WARN: The id must be an integer. Try again!"
+         return
+        }
+        # search if id exists
+        Search_func && {
+         echo "${YELLOW}INFO: The id '$id' already exists on database!"
+         return
+        }
+        # if id not exists, go ahead
+        echo -n "Enter the new id: "
+        read id
+        echo -n "Enter complete name: "
+        read name
+        echo -n "Enter login [first name.last name]: "
+        read login
+        echo -n "Enter age [0-99]: "
+        read age
+        echo -n "Enter gender [male|female]: "
+        read gender
+        echo -n "Enter job title: "
+        read job
+        echo -n "Enter department: "
+        read department
+        echo
+        Insert_func "$id:$name:$login:$age:$gender:$job:$department"
+        echo
+  ;;
+  remove )
+        local all_users=$(cat "$DB_FILE" | column -t -s $SEP)
+        echo "DataTex users list:"
+        echo "$all_users"
+        echo
+        echo
+        echo -n "Which ID do you want to remove? "
+        read id
+        echo
+
+           if Search_func "$id" ; then
+             Remove_func "$id"
+           else
+             echo "${YELLOW}INFO: The id '$id' not exists on Database!"
+           fi
+             echo
+  ;;
+       * )
+       echo "${YELLOW}WARN: Invalid option: '$1'!"
+       return 1
+  ;;
 esac
