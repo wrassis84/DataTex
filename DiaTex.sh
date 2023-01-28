86b0058d9decf4bb402c5ef5d2ac796c3336912a
William Rezende <wrassis84@gmail.com>
2023-01-28 15:38:04 -0300

    refactor: remove some unused options vars

--- DiaTex.sh
+++ DiaTex.sh
@@ -20,12 +20,6 @@
 #
 DB_FILE="DataDB.txt" #| Database file
 LIB_FILE="LibTex.sh" #| Library file
-RED="\033[31;1m"     #|
-GREEN="\033[32;1m"   #|
-YELLOW="\033[33;1m"  #| Colors for output:
-PURPLE="\033[35;1m"  #|
-CIAN="\033[36;1m"    #|
-ESC="\033[m"         #| ESCAPE character
 #
 ### MAIN CODE ##################################################################
 #
